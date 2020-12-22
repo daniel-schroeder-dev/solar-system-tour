09d93892555e45698fe4106afb6241218aece036
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-12-22 12:17:21 -0600

    Toggle the visible attribute of the neptune-popup.

--- trigger-next-animation.js
+++ trigger-next-animation.js
@@ -1,7 +1,9 @@
 AFRAME.registerComponent('trigger-next-animation', {
   schema: {},
   init: function() {
-  	this.el.addEventListener('animationcomplete', e => console.log(e));
+  	this.el.addEventListener('animationcomplete', e => {
+  		document.querySelector('#neptune-popup').setAttribute('visible', true);
+  	});
   },
   update: function () {},
   tick: function () {},
